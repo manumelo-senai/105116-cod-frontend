9ae5a7fa41e9f3fe89bef1050ee62db9fdd2addc
manumelo-senai <manu.melo@ba.estudante.senai.br>
2026-08-19 10:14:46 -0300

    estilização avançada, com css e javascript

--- projeto-burguer-house/app/src/script.js
+++ projeto-burguer-house/app/src/script.js
@@ -1,7 +1,16 @@
 const menu = document.querySelector('.menu');
 const NavMenu = document.querySelector('.nav-menu');
 
-menu.addEventListener('click',() => {
+menu.addEventListener('click', () => {
     menu.classList.toggle('ativo');
     NavMenu.classList.toggle('ativo');
-})
+});
+
+document.addEventListener('mousemove', (e) => {
+    const x = (window.innerWidth / 2 - e.clientX) / 20;
+    const y = (window.innerHeight / 2 - e.clientY) / 20;
+
+    const img = document.querySelector('.home-img');
+
+    img.style.transform = `translate(${x}px, ${y}px)`;
+});
